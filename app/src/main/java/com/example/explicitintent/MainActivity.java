a5d09388e6443e2f811810211192aec69b66d73c
SmritiManikand <113674204+SmritiManikand@users.noreply.github.com>
2024-03-20 14:34:51 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.explicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculateFactorial(View view) {
+        EditText numberEditText = findViewById(R.id.numberEditText);
+        int number = Integer.parseInt(numberEditText.getText().toString());
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("number", number);
+        startActivity(intent);
     }
 }
